efa3e48d710f4a2c3112bb5ae211318e682bac84
Cesaly Garrett <cesalygarrett@gmail.com>
2020-05-08 22:07:44 -0500

    Finished the forms part of recipe box

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from recipe import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="homepage"),
+    path('recipeadd/', views.recipeadd),
     path('<int:id>/', views.detail_recipe),
     path('author/<int:id>/', views.detail_author),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 
 from recipe.models import RecipeItem, Author
+from recipe.forms import RecipeAddForm
 
 # Create your views here.
 
@@ -8,6 +9,26 @@ from recipe.models import RecipeItem, Author
 def index(request):
     data = RecipeItem.objects.all()
     return render(request, 'index.html', {'data': data})
+
+
+def recipeadd(request):
+    html = "recipeaddform.html"
+
+    if request.method == 'POST':
+        form = RecipeAddForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            RecipeItem.object.Create(
+                title=data['title'],
+                description=data['description'],
+                author=data['author']
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+
+
+    form = RecipeAddForm()
+
+    return render(request, html, {"form": form})
 
 
 def detail_recipe(request, id):
